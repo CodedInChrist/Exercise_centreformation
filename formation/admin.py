9876d5b2696c5481bd25fcdf2c4ae1076546665b
Centre Formation <user@example.com>
2026-02-10 01:13:03 +0200

    Updating models and admin files

--- formation/admin.py
+++ formation/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
-from .models import Salle, Session
+from .models import Salle, Formateur, Session
 
+admin.site.site_header = "Centre de Formation"
+
+@admin.register(Salle)
+class SalleAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'capacite', 'localisation', 'createur')
+    search_fields = ('nom', 'localisation')
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.createur = request.user  # CORRIGÉ : 'createur'
+        obj.save()
+
+@admin.register(Formateur)
+class FormateurAdmin(admin.ModelAdmin):
+    list_display = ('user', 'specialite')
+    search_fields = ('user__username', 'user__first_name')
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'salle', 'formateur', 'date_debut', 'date_fin', 'createur')
+    list_filter = ('salle', 'formateur')
+    search_fields = ('titre', 'salle__nom')
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.createur = request.user
+        obj.save()
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(createur=request.user)
--- formation/models.py
+++ formation/models.py
@@ -1,18 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class Salle(models.Model):
+    id = models.BigAutoField(primary_key=True)
     nom = models.CharField(max_length=100, unique=True)
-    capacite = models.IntegerField() 
-    description = models.TextField(blank=True, null=True)
+    capacite = models.PositiveIntegerField()
     localisation = models.CharField(max_length=200, blank=True)
+    createur = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     date_creation = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.nom} ({self.capacite} places)"
+
+class Formateur(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialite = models.CharField(max_length=200)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
 
 class Session(models.Model):
+    id = models.BigAutoField(primary_key=True)
     titre = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
     salle = models.ForeignKey(Salle, on_delete=models.CASCADE)
+    formateur = models.ForeignKey(Formateur, on_delete=models.CASCADE)
     date_debut = models.DateTimeField()
     date_fin = models.DateTimeField()
-    nombre_participants = models.IntegerField() 
-    formateur = models.CharField(max_length=100)
+    nombre_participants = models.PositiveIntegerField()
+    createur = models.ForeignKey(User, on_delete=models.PROTECT, default=1)
     date_creation = models.DateTimeField(auto_now_add=True)
+    
+    def clean(self):
+        if self.date_fin <= self.date_debut:
+            raise ValidationError("shiramwo umusi uri nyuma y'umusi wa mbere")
+        
+        if self.date_debut < timezone.now():
+            raise ValidationError("uyo musi wararenganye")
+        
+        if self.nombre_participants > self.salle.capacite:
+            raise ValidationError(f"Trop de participants pour {self.salle.nom}")
+        
+        conflits = Session.objects.filter(
+            salle=self.salle,
+            date_debut__lt=self.date_fin,
+            date_fin__gt=self.date_debut
+        )
+        
+        if self.pk:
+            conflits = conflits.exclude(pk=self.pk)
+        
+        if conflits.exists():
+            raise ValidationError("Salle yarafashwe")
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.titre} - {self.salle.nom}"
